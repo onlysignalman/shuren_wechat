c25e44a0ca82010296fec7e99faad3f094286af0
onlysignalman <1138756325@qq.com>
2017-05-18 20:49:24 +0800

    提交

---
+++ src/main/java/com/shuren/resume/controller/UserBaseinfoController.java
@@ -0,0 +1,10 @@
+package com.shuren.resume.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by 董帮辉 on 2017-5-18.
+ */
+@Controller
+public class UserBaseinfoController {
+}
