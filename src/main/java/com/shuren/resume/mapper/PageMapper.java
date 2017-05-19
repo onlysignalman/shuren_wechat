865002a3461870a08b2002436a7a16b2bf7de4d4
minghaozhi <597575122@qq.com>
2017-05-19 09:09:20 +0800

    change

--- src/main/java/com/shuren/resume/mapper/PageMapper.java
+++ src/main/java/com/shuren/resume/mapper/PageMapper.java
@@ -1,7 +1,10 @@
 package com.shuren.resume.mapper;
 
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.shuren.resume.pojo.Page;
+
 /**
  * Created by 董帮辉 on 2017-5-18.
  */
-public interface PageMapper {
+public interface PageMapper extends BaseMapper<Page> {
 }
