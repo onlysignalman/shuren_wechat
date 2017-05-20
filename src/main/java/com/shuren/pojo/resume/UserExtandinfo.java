d4903a782abc2b6d830c8c5c48c7c327dde82c6f
onlysignalman <1138756325@qq.com>
2017-05-20 23:43:50 +0800

    配置修改日志输出位置

---
+++ src/main/java/com/shuren/pojo/resume/UserExtandinfo.java
@@ -0,0 +1,513 @@
+package com.shuren.pojo.resume;
+
+/**
+ * Created by 董帮辉 on 2017-5-20.
+ * 用户扩展信息
+ */
+public class UserExtandinfo {
+
+    private Integer id;
+
+    //用户的Id
+    private Integer userId;
+
+    //用户头像地址
+    private String logo;
+
+    //用户信息完善分数
+    private Integer score;
+
+    //真实姓名
+    private String realName;
+
+    //性别
+    private String sex;
+
+    //出生日期时间戳
+    private Long birthday;
+
+    //身高
+    private String tall;
+
+    //体重kg
+    private String weight;
+
+    //民族
+    private String nation;
+
+    //政治面貌
+    private String politics;
+
+    //婚姻状况
+    private String marry;
+
+    //联系方式
+    private String contactPhone;
+
+    //联系邮箱
+    private String contactEmail;
+
+    //身份证号
+    private String idCard;
+
+    //户口所在地
+    private String residence;
+
+    //现住地
+    private String location;
+
+    //当前工作状态
+    private String nowStatus;
+
+    //期望工作性质
+    private String wishType;
+
+    //期望行业
+    private String wishBusiness;
+
+    //期望职业
+    private String wishJob;
+
+    //期望工作地点
+    private String wishLocation;
+
+    //期望薪资
+    private String wishSalary;
+
+    //学校名称
+    private String collegeName;
+
+    //专业名称
+    private String specialities;
+
+    //毕业时间
+    private String graduationTime;
+
+    //学校性质
+    private String collegeType;
+
+    //学历
+    private String diplomas;
+
+    //学位
+    private String degree;
+
+    //班级职务
+    private String classDuty;
+
+    //学生会职务
+    private String studentDuty;
+
+    //奖学金
+    private String scholarship;
+
+    //奖学金级别
+    private String scholarshipRange;
+
+    //奖项
+    private String awards;
+
+    //奖项级别
+    private String awardsRange;
+
+    //奖项描述
+    private String awardsDes;
+
+    //实习项目
+    private String internProject;
+
+    //实习时间
+    private String internTime;
+
+    //实习职务
+    private String internDuty;
+
+    //实习描述
+    private String internDesc;
+
+    //兴趣爱好
+    private String hobby;
+
+    //社会荣誉
+    private String honor;
+
+    //自我评价
+    private String assessment;
+
+    //创建时间
+    private Long createTimeStamp;
+
+    //最新更新时间
+    private Long lastUpdateTimeStamp;
+
+    //逻辑删除
+    private Integer isDel;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Long getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Long birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getTall() {
+        return tall;
+    }
+
+    public void setTall(String tall) {
+        this.tall = tall;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public String getPolitics() {
+        return politics;
+    }
+
+    public void setPolitics(String politics) {
+        this.politics = politics;
+    }
+
+    public String getMarry() {
+        return marry;
+    }
+
+    public void setMarry(String marry) {
+        this.marry = marry;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getResidence() {
+        return residence;
+    }
+
+    public void setResidence(String residence) {
+        this.residence = residence;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getNowStatus() {
+        return nowStatus;
+    }
+
+    public void setNowStatus(String nowStatus) {
+        this.nowStatus = nowStatus;
+    }
+
+    public String getWishType() {
+        return wishType;
+    }
+
+    public void setWishType(String wishType) {
+        this.wishType = wishType;
+    }
+
+    public String getWishBusiness() {
+        return wishBusiness;
+    }
+
+    public void setWishBusiness(String wishBusiness) {
+        this.wishBusiness = wishBusiness;
+    }
+
+    public String getWishJob() {
+        return wishJob;
+    }
+
+    public void setWishJob(String wishJob) {
+        this.wishJob = wishJob;
+    }
+
+    public String getWishLocation() {
+        return wishLocation;
+    }
+
+    public void setWishLocation(String wishLocation) {
+        this.wishLocation = wishLocation;
+    }
+
+    public String getWishSalary() {
+        return wishSalary;
+    }
+
+    public void setWishSalary(String wishSalary) {
+        this.wishSalary = wishSalary;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public String getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(String specialities) {
+        this.specialities = specialities;
+    }
+
+    public String getGraduationTime() {
+        return graduationTime;
+    }
+
+    public void setGraduationTime(String graduationTime) {
+        this.graduationTime = graduationTime;
+    }
+
+    public String getCollegeType() {
+        return collegeType;
+    }
+
+    public void setCollegeType(String collegeType) {
+        this.collegeType = collegeType;
+    }
+
+    public String getDiplomas() {
+        return diplomas;
+    }
+
+    public void setDiplomas(String diplomas) {
+        this.diplomas = diplomas;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getClassDuty() {
+        return classDuty;
+    }
+
+    public void setClassDuty(String classDuty) {
+        this.classDuty = classDuty;
+    }
+
+    public String getStudentDuty() {
+        return studentDuty;
+    }
+
+    public void setStudentDuty(String studentDuty) {
+        this.studentDuty = studentDuty;
+    }
+
+    public String getScholarship() {
+        return scholarship;
+    }
+
+    public void setScholarship(String scholarship) {
+        this.scholarship = scholarship;
+    }
+
+    public String getScholarshipRange() {
+        return scholarshipRange;
+    }
+
+    public void setScholarshipRange(String scholarshipRange) {
+        this.scholarshipRange = scholarshipRange;
+    }
+
+    public String getAwards() {
+        return awards;
+    }
+
+    public void setAwards(String awards) {
+        this.awards = awards;
+    }
+
+    public String getAwardsRange() {
+        return awardsRange;
+    }
+
+    public void setAwardsRange(String awardsRange) {
+        this.awardsRange = awardsRange;
+    }
+
+    public String getAwardsDes() {
+        return awardsDes;
+    }
+
+    public void setAwardsDes(String awardsDes) {
+        this.awardsDes = awardsDes;
+    }
+
+    public String getInternProject() {
+        return internProject;
+    }
+
+    public void setInternProject(String internProject) {
+        this.internProject = internProject;
+    }
+
+    public String getInternTime() {
+        return internTime;
+    }
+
+    public void setInternTime(String internTime) {
+        this.internTime = internTime;
+    }
+
+    public String getInternDuty() {
+        return internDuty;
+    }
+
+    public void setInternDuty(String internDuty) {
+        this.internDuty = internDuty;
+    }
+
+    public String getInternDesc() {
+        return internDesc;
+    }
+
+    public void setInternDesc(String internDesc) {
+        this.internDesc = internDesc;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getHonor() {
+        return honor;
+    }
+
+    public void setHonor(String honor) {
+        this.honor = honor;
+    }
+
+    public String getAssessment() {
+        return assessment;
+    }
+
+    public void setAssessment(String assessment) {
+        this.assessment = assessment;
+    }
+
+    public Long getCreateTimeStamp() {
+        return createTimeStamp;
+    }
+
+    public void setCreateTimeStamp(Long createTimeStamp) {
+        this.createTimeStamp = createTimeStamp;
+    }
+
+    public Long getLastUpdateTimeStamp() {
+        return lastUpdateTimeStamp;
+    }
+
+    public void setLastUpdateTimeStamp(Long lastUpdateTimeStamp) {
+        this.lastUpdateTimeStamp = lastUpdateTimeStamp;
+    }
+
+    public Integer getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Integer isDel) {
+        this.isDel = isDel;
+    }
+}
