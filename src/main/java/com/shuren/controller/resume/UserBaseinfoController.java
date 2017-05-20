02768fcdedc2d43f396f790046a87e6e697fb1eb
Xuwenee <Xuwen123>
2017-05-20 19:19:33 +0800

    提交分支

--- src/main/java/com/shuren/controller/resume/UserBaseinfoController.java
+++ src/main/java/com/shuren/controller/resume/UserBaseinfoController.java
@@ -1,7 +1,10 @@
 package com.shuren.controller.resume;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shuren.service.resume.UserBaseinfoService;
 
 /**
  * Created by 董帮辉 on 2017-5-18.
@@ -9,4 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/userBaseinfo")
 public class UserBaseinfoController {
+	
+	@Autowired
+	private UserBaseinfoService userBaseinfoService;
+	
+	
+	@RequestMapping("/register")
+	public void register(){
+		
+	}
+	
+	
+	
+	@RequestMapping("/login")
+	public void login(){
+		
+		
+		
+	}
+	
 }
